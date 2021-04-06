fa2ece776ef451e5c4bcd8da0b908972f3488bf5
aa <angelopoulos@berkeley.edu>
2021-04-05 20:34:20 -0700

    [plot the alphaplus quantity]

--- scripts/concentration.py
+++ scripts/concentration.py
@@ -1,8 +1,12 @@
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats as stats
 from scipy.optimize import brentq
 import pdb
+from pathlib import Path
+
+CACHE = str(Path(__file__).parent.absolute()) + '/.cache/'
 
 def safe_min(x):
     if np.any(np.isnan(x)):
@@ -16,7 +20,8 @@ def expand_grid(arr1,arr2):
     return params[1].T.flatten(), params[0].T.flatten()
 
 # Get t
-def normalized_vapnik_tail_upper(n, m, alpha, eta, maxiter):
+# Equation 12 in Lihua's note 
+def normalized_vapnik_tail_upper(n, m, delta, eta, maxiter):
     c1 = np.log(1 / 4 / (1-stats.norm.cdf(np.sqrt(2))) )
     c2 = 5 * np.sqrt( 2*np.pi*np.exp(1) ) * ( 2*stats.norm.cdf(1) - 1)
     def _tailprob(x):
@@ -39,11 +44,12 @@ def normalized_vapnik_tail_upper(n, m, alpha, eta, maxiter):
         log_prob_pinelis = safe_min(np.log(gauss + stats.norm.pdf(tmp)/(9+tmp**2)*c2) + extra_term)
         log_prob_hoeffding = safe_min(-tmp**2/2 + extra_term)
         log_prob = np.min([log_prob_bardenet, log_prob_bentkus_dzindzalieta, log_prob_pinelis, log_prob_hoeffding])
-        return log_prob - np.log(alpha)
+        return log_prob - np.log(delta)
 
     return brentq(_tailprob,0,1,maxiter=maxiter) 
 
-def normalized_vapnik_tail_lower(n, m, alpha, eta, maxiter):
+# Equation 11 in Lihua's note.
+def normalized_vapnik_tail_lower(n, m, delta, eta, maxiter):
     c1 = np.log(1 / 4 / (1-stats.norm.cdf(np.sqrt(2))) )
     c2 = 5 * np.sqrt( 2*np.pi*np.exp(1) ) * ( 2*stats.norm.cdf(1) - 1)
     def _tailprob(x):
@@ -66,20 +72,20 @@ def normalized_vapnik_tail_lower(n, m, alpha, eta, maxiter):
         log_prob_pinelis = safe_min(np.log(gauss + stats.norm.pdf(tmp)/(9+tmp**2)*c2) + extra_term)
         log_prob_hoeffding = safe_min(-tmp**2/2 + extra_term)
         log_prob = np.min([log_prob_bardenet, log_prob_bentkus_dzindzalieta, log_prob_pinelis, log_prob_hoeffding])
-        return log_prob - np.log(alpha)
+        return log_prob - np.log(delta)
 
     return brentq(_tailprob,0,1,maxiter=maxiter) 
 
-# Return required fdr
-def shat_upper_tail(s, n, m, alpha, eta, maxiter):
-    t = normalized_vapnik_tail_upper(n, m, alpha, eta, maxiter)
+# Return upper bound fdr
+def shat_upper_tail(s, n, m, delta, eta, maxiter):
+    t = normalized_vapnik_tail_upper(n, m, delta, eta, maxiter)
     def _condition(shat):
         return (shat - s)/np.sqrt(shat + eta) - t
     shat = brentq(_condition,0,1,maxiter=maxiter) 
     return shat
 
-def shat_lower_tail(s, n, m, alpha, eta, maxiter):
-    t = normalized_vapnik_tail_lower(n, m, alpha, eta, maxiter)
+def shat_lower_tail(s, n, m, delta, eta, maxiter):
+    t = normalized_vapnik_tail_lower(n, m, delta, eta, maxiter)
     def _condition(shat):
         return (s - shat)/np.sqrt(s + eta) - t
     try:
@@ -89,22 +95,51 @@ def shat_lower_tail(s, n, m, alpha, eta, maxiter):
         shat = 0
     return shat
 
+# Return required fdp needed to achieve an fdr of alpha
+def required_fdp(n, m, alpha, delta, maxiter):
+    eta_star = get_eta_star_upper(n, m, alpha, delta, maxiter)
+    t = normalized_vapnik_tail_upper(n, m, delta, eta_star, maxiter)
+    def _condition(alpha_plus):
+        return alpha - (alpha_plus + t * np.sqrt(alpha_plus + eta_star))
+    try:
+        alpha_plus = brentq(_condition,0,1,maxiter=maxiter)
+    except:
+        print("Warning: setting alpha_plus to 0 due to failed search")
+        alpha_plus = 0
+    return alpha_plus 
+
+
+# Get optimal eta for upper tail
+def get_eta_star_upper(n, m, alpha, delta, maxiter):
+    fname = f'eta_star_{n}_{alpha}_{delta}'
+    fpath = CACHE+fname+'.npy'
+    if os.path.exists( fpath ):
+        return np.load( fpath )
+    else:
+        eta_grid = np.logspace(-5,1,100)
+        best_x = 0
+        eta_star = 1
+        for eta in eta_grid:
+            t = normalized_vapnik_tail_upper(n, m, delta, eta, maxiter)
+            x = 0.5*(t*np.sqrt(4*alpha+4*eta+t*t) + 2*alpha + t*t)
+            if x >= best_x:
+                best_x = x
+                eta_star = eta
+        os.makedirs(CACHE, exist_ok=True)
+        np.save( fpath, eta_star )
+        return eta_star
+
 if __name__ == "__main__":
     s = 0.2
     ns = np.logspace(2,5,100)
     m = 1000
-    alpha = 0.2
-    eta = 0.1
+    delta = 0.2
+    alpha = 0.1
     maxiter = 100000
+    eta_star_upper = get_eta_star_upper(ns[0], m, alpha, delta, maxiter)
 
     # Plot upper tail
-    shats = [shat_upper_tail(s, n, m, alpha, eta, maxiter) for n in ns]
-    plt.plot(ns,shats)
-    plt.xscale('log')
-    plt.savefig('../outputs/shat_upper_tail.pdf')
-
-    # Plot lower tail
-    shats = [shat_lower_tail(s, n, m, alpha, eta, maxiter) for n in ns]
+    shats = [shat_upper_tail(s, n, m, delta, eta_star_upper, maxiter) for n in ns]
     plt.plot(ns,shats)
     plt.xscale('log')
     plt.savefig('../outputs/shat_upper_tail.pdf')
