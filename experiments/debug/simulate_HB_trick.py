e7125dfec6efb8969bb639eb2d1dde4d46faaba5
aa <angelopoulos@berkeley.edu>
2021-05-24 01:00:17 -0700

    [debug HB simulation]

--- experiments/debug/simulate_HB_trick.py
+++ experiments/debug/simulate_HB_trick.py
@@ -1,9 +1,28 @@
 import os, sys, inspect
 sys.path.insert(1, os.path.join(sys.path[0], '../../'))
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from core.bounds import HB_mu_plus
+import pdb
 
 if __name__ == "__main__":
     n = 30000
-    alpha = 0.15
-    classifier_accuracy = 0.8
-
+    fdrs = np.logspace(-2,0,100)
+    alpha = 0.2
+    delta = 0.1
+    abstention_frequency = 0.6
+    maxiters = 1000
+    true_means = np.zeros_like(fdrs)
+    fdrpluses = np.zeros_like(fdrs)
+    for i in range(fdrs.shape[0]):
+        fdr = fdrs[i]
+        pvals = np.array([(1-fdr)*(1-abstention_frequency),abstention_frequency,fdr*(1-abstention_frequency)])
+        true_means[i] = (pvals * np.array([0,alpha,1])).sum()
+        mu = np.random.multinomial(n,pvals).dot(np.array([0,alpha,1]))/n
+        fdrpluses[i] = HB_mu_plus(mu, n, delta, maxiters) 
+    plt.plot(fdrs, fdrpluses-true_means, color='y', linewidth=3)
+    #plt.plot(fdrs, true_means, color='k',linewidth=3)
+    plt.ylim([0,None])
+    sns.despine(top=True,right=True)
+    plt.savefig("./outputs/HB_gap.pdf")
