2c5f7fc27d86a0df9f56babf8406bcc2713b0c00
aa <angelopoulos@berkeley.edu>
2021-08-30 17:09:43 -0700

    [keep ggoing]

--- experiments/detection/experiment.py
+++ experiments/detection/experiment.py
@@ -26,56 +26,73 @@ from UQHeads import UQHeads
 import pdb
 
 if __name__ == "__main__":
-    # Evaluations
-    annType = ['segm','bbox','keypoints']
-    annType = annType[0]      #specify type here
-    dataType = 'val2017'
-    prefix = 'person_keypoints' if annType=='keypoints' else 'instances'
-    dataDir='./datasets/coco/'
-    annFile = '%s/annotations/%s_%s.json'%(dataDir,prefix,dataType)
-    cocoGt=COCO(annFile)
+    with torch.no_grad():
+        # Evaluations
+        annType = ['segm','bbox','keypoints']
+        annType = annType[0]      #specify type here
+        dataType = 'val2017'
+        prefix = 'person_keypoints' if annType=='keypoints' else 'instances'
+        dataDir='./datasets/coco/'
+        annFile = '%s/annotations/%s_%s.json'%(dataDir,prefix,dataType)
+        cocoGt=COCO(annFile)
 
-    # Load the model
-    cfg = get_cfg()
-    # add project-specific config (e.g., TensorMask) here if you're not running a model in detectron2's core library
-    cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
-    cfg.MODEL.ROI_HEADS.NAME = "UQHeads"
-    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
-    # Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
-    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
-    predictor = DefaultPredictor(cfg)
+        # Load the model
+        cfg = get_cfg()
+        # add project-specific config (e.g., TensorMask) here if you're not running a model in detectron2's core library
+        cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+        cfg.MODEL.ROI_HEADS.NAME = "UQHeads"
+        cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
+        # Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
+        cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+        predictor = DefaultPredictor(cfg)
 
-    # get all images containing given categories, select one at random
-    for img_id in cocoGt.imgs:
-        img_metadata = cocoGt.loadImgs(img_id)[0]
-        img = io.imread('%s/%s/%s'%(dataDir,dataType,img_metadata['file_name']))
-        ann_ids = cocoGt.getAnnIds(imgIds=[img_id,])
-        anns = cocoGt.loadAnns(ann_ids)
-        try:
-            outputs = predictor(img)
-        except:
-            continue
-        pred_masks = outputs['instances'].pred_masks # P x H x W tensor
-        
+        # get all images and annotations 
+        pred_classes = []
+        pred_masks = []
+        pred_sets = []
+        gt_classes = []
         gt_masks = []
-        for ann in anns:
+        k=0
+        for img_id in cocoGt.imgs:
+            img_metadata = cocoGt.loadImgs(img_id)[0]
+            img = io.imread('%s/%s/%s'%(dataDir,dataType,img_metadata['file_name']))
+            ann_ids = cocoGt.getAnnIds(imgIds=[img_id,])
+            anns = cocoGt.loadAnns(ann_ids)
             try:
-                rleobj = maskUtils.frPyObjects([ann['segmentation'][0]],img_metadata['height'],img_metadata['width'])
+                outputs = predictor(img)
             except:
-                rleobj = maskUtils.frPyObjects([ann['segmentation']],img_metadata['height'],img_metadata['width'])
-            gt_masks = gt_masks + [maskUtils.decode(rleobj),]
-        try:
-            gt_masks = torch.tensor(np.concatenate(gt_masks,axis=2)).permute(2,0,1)
-        except:
-            continue
+                print(f"Image {img_id} didn't work.")
+                continue
+            
+            gt_masks_singleimage = []
+            for ann in anns:
+                try:
+                    rleobj = maskUtils.frPyObjects([ann['segmentation'][0]],img_metadata['height'],img_metadata['width'])
+                except:
+                    rleobj = maskUtils.frPyObjects([ann['segmentation']],img_metadata['height'],img_metadata['width'])
+                gt_masks_singleimage = gt_masks_singleimage + [maskUtils.decode(rleobj),]
+            try:
+                gt_masks_singleimage = torch.tensor(np.concatenate(gt_masks_singleimage,axis=2)).permute(2,0,1)
+            except:
+                continue
+            pred_classes = pred_classes + [outputs['instances'].pred_classes,]
+            pred_masks = pred_masks + [outputs['instances'].pred_masks,]
+            pred_sets = pred_sets + [outputs['instances'].pred_sets,]
+            gt_classes = gt_classes + [torch.tensor([ann['category_id']-1 for ann in anns]),]
+            gt_masks = gt_masks + [gt_masks_singleimage,] 
+            print(f"Image {img_id} worked!")
+            k = k + 1
+            if k == 5:
+                pdb.set_trace()
+                break
 
-    # We can use `Visualizer` to draw the predictions on the image.
-    v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
-    out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+        # We can use `Visualizer` to draw the predictions on the image.
+        v = Visualizer(img[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
+        out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
 
-    plt.figure()
-    plt.imshow(out.get_image()[:, :, ::-1])
-    plt.savefig('output.jpg')
+        plt.figure()
+        plt.imshow(out.get_image()[:, :, ::-1])
+        plt.savefig('output.jpg')
 
     # TODO:  See https://github.com/cocodataset/cocoapi/blob/master/PythonAPI/pycocoEvalDemo.ipynb
     # running evaluation
