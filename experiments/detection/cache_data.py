356b7ea36c8b75d3aeea5b32d749a51695590c7d
aa <angelopoulos@berkeley.edu>
2021-09-02 12:26:12 -0700

    [exp]

--- experiments/detection/cache_data.py
+++ experiments/detection/cache_data.py
@@ -43,7 +43,7 @@ if __name__ == "__main__":
         # add project-specific config (e.g., TensorMask) here if you're not running a model in detectron2's core library
         cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
         cfg.MODEL.ROI_HEADS.NAME = "UQHeads"
-        cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.1 #set threshold for this model
+        cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5 #set threshold for this model
         # Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
         cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
         predictor = DefaultPredictor(cfg)
--- experiments/detection/experiment.py
+++ experiments/detection/experiment.py
@@ -11,7 +11,21 @@ import pdb
 def eval_image(roi_mask, box, softmax_output, gt_classes, gt_masks, threshold):
     pred_masks = roi_mask.to_bitmasks(box,gt_masks.shape[1],gt_masks.shape[2],threshold).tensor
 
+    if softmax_output.shape[0] == 0:
+       return None, None, None 
+
     top_scores, indices = softmax_output.max(dim=1)[0].sort(descending=True)
+    filter_idx = top_scores > 0.5 
+
+    softmax_output = softmax_output[filter_idx]
+    box = box[filter_idx]
+    roi_mask = roi_mask[filter_idx]
+    # must reindex
+    if softmax_output.shape[0] == 0:
+       return None, None, None 
+
+    top_scores, indices = softmax_output.max(dim=1)[0].sort(descending=True)
+
     est_classes = softmax_output.argmax(dim=1)
 
     # Starting with the most confident mask, correspond it with a ground truth mask based on IOU
@@ -28,7 +42,7 @@ def eval_image(roi_mask, box, softmax_output, gt_classes, gt_masks, threshold):
         corrects[index] = gt_classes[unused][max_iou_idx] == est_classes[index] 
         unused = unused[unused != unused[max_iou_idx]]
 
-    return corrects, ious 
+    return corrects, ious, unused 
 
 if __name__ == "__main__":
     with torch.no_grad():
@@ -51,5 +65,15 @@ if __name__ == "__main__":
     lambda1s = torch.linspace(0,1,100) # Top score threshold
     lambda2s = torch.linspace(0,1,100) # Segmentation threshold
     lambda3s = torch.linspace(0,1,1000) # APS threshold
+    running_corrects = 0
+    running_total = 0
+    running_gt = 0
     for i in range(len(roi_masks)):
-        corrects, ious = eval_image(roi_masks[i],boxes[i],softmax_outputs[i],gt_classes[i],gt_masks[i],0.5)
+        corrects, ious, unused = eval_image(roi_masks[i],boxes[i],softmax_outputs[i],gt_classes[i],gt_masks[i],0.5)
+        if corrects == None:
+            continue
+        running_corrects += ((corrects + (ious > 0.5)).float() >= 2).float().sum()
+        running_total += float(corrects.shape[0])
+        running_gt += float(gt_masks[i].shape[0])
+        print(f"The precision is: {running_corrects/running_total}")
+        print(f"The recall is: {running_corrects/running_gt}")
