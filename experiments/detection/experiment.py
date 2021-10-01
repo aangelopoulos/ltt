9343d6e9e97ff2b6658bded4acfd13944456ec8d
aa <angelopoulos@berkeley.edu>
2021-10-01 15:32:04 -0700

    [checkpoint]

--- experiments/detection/experiment.py
+++ experiments/detection/experiment.py
@@ -47,7 +47,7 @@ def plot(df,alphas):
     os.makedirs("./outputs/histograms/",exist_ok=True)
     plt.savefig("./" + f"outputs/histograms/detector_{alphas[0]}_{alphas[1]}_delta_histograms".replace(".","_") + ".pdf")
 
-def calculate_corrected_p_values(calib_tables, alphas):
+def calculate_all_p_values(calib_tables, alphas):
     n = calib_tables.shape[0]
     # Get p-values for each loss
     r_hats = calib_tables.mean(axis=0).squeeze().flatten(start_dim=1) # empirical risk at each lambda combination
@@ -56,7 +56,11 @@ def calculate_corrected_p_values(calib_tables, alphas):
         for i in range(p_values.shape[1]): 
             p_values[r,i] = hb_p_value(r_hats[r,i],n,alphas[r])
 
+    return p_values
+
+def calculate_corrected_p_values(calib_tables, alphas):
     # Combine them
+    p_values = calculate_all_p_values(calib_tables, alphas)
     p_values_corrected = p_values.max(axis=0)
     return p_values_corrected
     
@@ -67,7 +71,36 @@ def flatten_lambda_meshgrid(lambda1s,lambda2s,lambda3s):
     l3_meshgrid = l3_meshgrid.flatten()
     return l1_meshgrid, l2_meshgrid, l3_meshgrid
 
-def trial(i, alphas, delta, lambda1s, lambda2s, lambda3s, l1_meshgrid, l2_meshgrid, l3_meshgrid, num_calib, loss_tables, risks, lhats):
+def split_fixed_sequence(calib_tables, alphas, delta):
+    # Split the data
+    n_calib = calib_tables.shape[0]
+    n_coarse = n_calib//2
+    perm = torch.randperm(n_calib)
+    calib_tables = calib_tables[perm]
+    coarse_tables, fine_tables = (calib_tables[:n_coarse],calib_tables[n_coarse:])
+    p_values_coarse = calculate_all_p_values(coarse_tables, alphas)
+    # Find a lambda for each value of beta that controls the risk best.
+    num_betas = 200 
+    betas = np.logspace(-9,0,num_betas)
+    lambda_sequence = np.zeros_like(betas)
+    for b in range(num_betas):
+        beta = betas[b]
+        differences = np.abs(p_values_coarse - beta)
+        infty_norms = np.linalg.norm(differences, ord=np.inf, axis=0)
+        lambda_sequence[b] = infty_norms.argmin()
+
+    _, idx = np.unique(lambda_sequence, return_index=True)
+    lambda_sequence_ordered = lambda_sequence[np.sort(idx)]
+    pdb.set_trace()
+
+    # Now test these lambdas 
+    fine_tables = fine_tables.flatten(start_dim=2)[:,:,lambda_sequence_ordered]
+    p_values_fine = calculate_corrected_p_values(fine_tables, alphas)
+    rejections = lambda_sequence_ordered[np.nonzero(p_values_fine < delta)[0]].astype(int)
+
+    return rejections 
+
+def trial(i, method, alphas, delta, lambda1s, lambda2s, lambda3s, l1_meshgrid, l2_meshgrid, l3_meshgrid, num_calib, loss_tables, risks, lhats):
     fix_randomness(seed=(i*10000))
     n = loss_tables["tensor"].shape[0]
     perm = torch.randperm(n)
@@ -75,8 +108,12 @@ def trial(i, alphas, delta, lambda1s, lambda2s, lambda3s, l1_meshgrid, l2_meshgr
     local_tables = loss_tables["tensor"][perm]
     calib_tables, val_tables = (local_tables[:num_calib], local_tables[num_calib:])
 
-    p_values_corrected = calculate_corrected_p_values(calib_tables, alphas)
-    R = bonferroni(p_values_corrected, delta)
+    if method == "Bonferroni":
+        p_values_corrected = calculate_corrected_p_values(calib_tables, alphas)
+        R = bonferroni(p_values_corrected, delta)
+
+    elif method == "Split Fixed Sequence":
+        R = split_fixed_sequence(calib_tables, alphas, delta)
 
     if R.shape[0] == 0:
         lhats[i] = np.array([1.0,1.0,1.0])
@@ -88,9 +125,6 @@ def trial(i, alphas, delta, lambda1s, lambda2s, lambda3s, l1_meshgrid, l2_meshgr
     l2s = l2_meshgrid[R]
     l3s = l3_meshgrid[R]
     
-    #l1 = l1s[l3s > l1s].min()
-    #l2 = l2s[(l3s > l1s) & (l1s == l1)].median()
-    #l3 = l3s[(l3s > l1s) & (l1s == l1) & (l2s == l2)].min()
     l3 = l3s[l3s > l1s].min()
     l2 = l2s[(l3s > l1s) & (l3s==l3)].median()
     l1 = l1s[(l3s > l1s) & (l2s==l2) & (l3s==l3)].min()
@@ -126,61 +160,62 @@ if __name__ == "__main__":
     manager = mp.Manager()
     loss_tables = manager.dict({"tensor": None, "curr_proc": 0})
 
-    fname = f'./.cache/{alphas}_{delta}_{num_calib}_{num_trials}_dataframe.pkl'
+    for method in ["Bonferroni", "Split Fixed Sequence"]:
+        fname = f'./.cache/{method}_{alphas}_{delta}_{num_calib}_{num_trials}_dataframe.pkl'
+        try:
+            df = pd.read_pickle(fname)
+        except FileNotFoundError:
+            with torch.no_grad():
+                # Load cache
+                with open('./.cache/loss_tables.pt', 'rb') as f:
+                    #loss_tables["tensor"] = torch.tensor(np.random.random(size=(num_calib*2,3,lambda1s.shape[0],lambda2s.shape[0],lambda3s.shape[0])))/10
+                    loss_tables["tensor"] = torch.load(f)
 
-    try:
-        df = pd.read_pickle(fname)
-    except FileNotFoundError:
-        with torch.no_grad():
-            # Load cache
-            with open('./.cache/loss_tables.pt', 'rb') as f:
-                #loss_tables["tensor"] = torch.tensor(np.random.random(size=(num_calib*2,3,lambda1s.shape[0],lambda2s.shape[0],lambda3s.shape[0])))/10
-                loss_tables["tensor"] = torch.load(f)
+                risks = manager.dict({k:np.zeros((3,)) for k in range(num_trials)})
+                lhats = manager.dict({k:np.zeros((3,)) for k in range(num_trials)})
+                l1_meshgrid, l2_meshgrid, l3_meshgrid = flatten_lambda_meshgrid(lambda1s,lambda2s,lambda3s)
 
-            risks = manager.dict({k:np.zeros((3,)) for k in range(num_trials)})
-            lhats = manager.dict({k:np.zeros((3,)) for k in range(num_trials)})
-            l1_meshgrid, l2_meshgrid, l3_meshgrid = flatten_lambda_meshgrid(lambda1s,lambda2s,lambda3s)
+                # Test trial
+                trial(0, method, alphas, delta, lambda1s, lambda2s, lambda3s, l1_meshgrid, l2_meshgrid, l3_meshgrid, num_calib, loss_tables, risks, lhats)
+                # Queue the jobs
+                jobs = []
+                for i in range(num_trials):
+                    p = mp.Process(target = trial, args = (i, method, alphas, delta, lambda1s, lambda2s, lambda3s, l1_meshgrid, l2_meshgrid, l3_meshgrid, num_calib, loss_tables, risks, lhats)) 
+                    jobs.append(p)
 
-            # Queue the jobs
-            jobs = []
-            for i in range(num_trials):
-                p = mp.Process(target = trial, args = (i, alphas, delta, lambda1s, lambda2s, lambda3s, l1_meshgrid, l2_meshgrid, l3_meshgrid, num_calib, loss_tables, risks, lhats)) 
-                jobs.append(p)
+                pbar = tqdm(total=num_trials)
 
-            pbar = tqdm(total=num_trials)
+                # Run the jobs
+                for proc in jobs:
+                    while loss_tables["curr_proc"] >= num_processes:
+                        time.sleep(2)
+                    proc.start()
+                    loss_tables["curr_proc"] += 1
+                    pbar.update(1)
 
-            # Run the jobs
-            for proc in jobs:
-                while loss_tables["curr_proc"] >= num_processes:
-                    time.sleep(2)
-                proc.start()
-                loss_tables["curr_proc"] += 1
-                pbar.update(1)
+                pbar.close()
 
-            pbar.close()
+                for proc in jobs:
+                    proc.join()
 
-            for proc in jobs:
-                proc.join()
-
-            # Form the large dataframe
-            local_df_list = []
-            for i in tqdm(range(num_trials)):
-                dict_local = {"$\\hat{\\lambda}$": [lhats[i],],
-                              "mean coverage": 1-risks[i][0].item(),
-                              "mIOU": 1-risks[i][1].item(),
-                              "recall": 1-risks[i][2].item(),
-                              "alpha1": alphas[0],
-                              "alpha2": alphas[1],
-                              "alpha3": alphas[2],
-                              "delta": delta,
-                              "index": [0],
-                             }
-                df_local = pd.DataFrame(dict_local)
-                local_df_list = local_df_list + [df_local]
-            df = pd.concat(local_df_list, axis=0, ignore_index=True)
-            df.to_pickle(fname)
-    average_lambda = np.concatenate([arr[None,:] for arr in df["$\\hat{\\lambda}$"].tolist()],axis=0).mean(axis=0)
-    print(f"The average lambda_hat from the runs was: {list(average_lambda)}!")
-    pdb.set_trace()
-    plot(df,alphas)
-    print("Done!")
+                # Form the large dataframe
+                local_df_list = []
+                for i in tqdm(range(num_trials)):
+                    dict_local = {"$\\hat{\\lambda}$": [lhats[i],],
+                                  "mean coverage": 1-risks[i][0].item(),
+                                  "mIOU": 1-risks[i][1].item(),
+                                  "recall": 1-risks[i][2].item(),
+                                  "alpha1": alphas[0],
+                                  "alpha2": alphas[1],
+                                  "alpha3": alphas[2],
+                                  "delta": delta,
+                                  "index": [0],
+                                 }
+                    df_local = pd.DataFrame(dict_local)
+                    local_df_list = local_df_list + [df_local]
+                df = pd.concat(local_df_list, axis=0, ignore_index=True)
+                df.to_pickle(fname)
+        average_lambda = np.concatenate([arr[None,:] for arr in df["$\\hat{\\lambda}$"].tolist()],axis=0).mean(axis=0)
+        print(f"{method}: the average lambda_hat from the runs was: {list(average_lambda)}!")
+        plot(df,alphas)
+        print("Done!")
