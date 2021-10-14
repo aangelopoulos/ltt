2a3785ca7d75dd71e152a2ed272224e4856ff01d
aa <angelopoulos@berkeley.edu>
2021-10-14 11:36:06 -0700

    [change APS definition]

--- experiments/detection/demo.py
+++ experiments/detection/demo.py
@@ -57,6 +57,7 @@ if __name__ == "__main__":
         cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
         cfg.MODEL.ROI_HEADS.NAME = "UQHeads"
         cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5 #set threshold for this model
+        cfg.MODEL.ROI_HEADS.APS_THRESH = 0.999 #set threshold for this model
         # Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
         cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
         predictor = DefaultPredictor(cfg)
